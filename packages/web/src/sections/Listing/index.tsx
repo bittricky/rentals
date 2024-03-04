a780a03dc5abd82ca82065f79e04373b4218e418
Mitul Patel <me@mitulpa.tel>
2024-03-04 14:53:29 -0500

    fix(component): trailing bracket

--- packages/web/src/sections/Listing/index.tsx
+++ packages/web/src/sections/Listing/index.tsx
@@ -3,37 +3,43 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { Box } from "@chakra-ui/react";
 
-import { ErrorBanner, Skeleton } from '../../components';
+import { ErrorBanner, Skeleton } from "../../components";
 import { LISTING } from "../../graphql/queries";
-import { Listing as ListingData, ListingVariables } from "../../graphql/queries/Listing/__generated__/Listing";}
+import {
+  Listing as ListingData,
+  ListingVariables,
+} from "../../graphql/queries/Listing/__generated__/Listing";
 
 const PAGE_LIMIT = 3;
 
 export const Listing = () => {
   const [bookingsPage, setBookingsPage] = useState(1);
   const { id: listingId } = useParams();
-  const { loading, data, error } = useQuery<ListingData, ListingVariables>(LISTING, {
-    variables: {
-      id: listingId as string,
-      bookingsPage,
-      limit: PAGE_LIMIT
+  const { loading, data, error } = useQuery<ListingData, ListingVariables>(
+    LISTING,
+    {
+      variables: {
+        id: listingId as string,
+        bookingsPage,
+        limit: PAGE_LIMIT,
+      },
     }
-  })
+  );
 
   if (loading) {
     return (
       <Box>
         <Skeleton />
       </Box>
-    )
+    );
   }
 
   if (error) {
     return (
       <Box>
-       <ErrorBanner description="This listing may not exist or we've encountered an error. Please try again soon!" />
+        <ErrorBanner description="This listing may not exist or we've encountered an error. Please try again soon!" />
       </Box>
-    )
+    );
   }
 
   const listing = data ? data.listing : null;
