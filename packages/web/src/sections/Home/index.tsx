fbf82c117361b505bc4b4fca34b22f12d85c8a1a
Mitul Patel <me@mitulpa.tel>
2024-03-06 14:08:39 -0500

    feat(component): add popular listings link

--- packages/web/src/sections/Home/index.tsx
+++ packages/web/src/sections/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
 import { HomeHero } from "./components";
 
 // Assuming mapBackground is imported or defined elsewhere
@@ -20,6 +20,8 @@ export const Home = () => {
     }
   };
 
+  const onPopularSearchClick = () => navigate("/listings/united%20states");
+
   return (
     <Box bgImage={`url(${mapBackground})`} bgPos="center" bgSize="cover">
       <HomeHero onSearch={onSearch} />
@@ -31,6 +33,9 @@ export const Home = () => {
           Helping you make the best decisions in renting your last minute
           locations.
         </Text>
+        <Button onClick={onPopularSearchClick} colorScheme="blue" size="lg">
+          Popular listings in the United States
+        </Button>
       </Box>
     </Box>
   );
