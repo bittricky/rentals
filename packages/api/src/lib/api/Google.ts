9a38fc7065a7f1882cdc949b386bce07e055af15
Mitul Patel <me@mitulpa.tel>
2024-03-10 15:29:47 -0400

    feat(utils): wrote geocoding google handler

---
+++ packages/api/src/lib/api/Google.ts
@@ -0,0 +1,50 @@
+import {
+  AddressComponent,
+  AddressType,
+  Client,
+  GeocodingAddressComponentType,
+} from "@googlemaps/google-maps-services-js";
+
+if (!process.env.G_GEOCODE_KEY) {
+  throw new Error("[app] Missing Google Maps API key");
+}
+
+const maps = new Client({});
+
+const parseAddress = (addressComponents: AddressComponent[]) => {
+  const address = addressComponents.reduce(
+    (acc, component) => {
+      if (component.types.includes(AddressType.country))
+        acc.country = component.long_name;
+      if (component.types.includes(AddressType.administrative_area_level_1))
+        acc.administrativeArea = component.long_name;
+      if (
+        component.types.includes(AddressType.locality) ||
+        component.types.includes(GeocodingAddressComponentType.postal_town)
+      )
+        acc.city = component.long_name;
+
+      return acc;
+    },
+    { country: "", administrativeArea: "", city: "" }
+  );
+
+  return address;
+};
+
+export const Google = {
+  geocode: async (address: string) => {
+    const res = await maps.geocode({
+      params: {
+        address,
+        key: process.env.G_GEOCODE_KEY!,
+      },
+    });
+
+    if (res.status < 200 || res.status > 299) {
+      throw new Error("[app] Failed to geocode address!");
+    }
+
+    return parseAddress(res.data.results[0].address_components);
+  },
+};
