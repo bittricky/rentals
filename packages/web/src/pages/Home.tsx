5d72279caf0428d94a8d3778e0db7cec969b7e05
Mitul Patel <me@mitulpa.tel>
2024-12-08 04:45:16 -0500

    feat(home): language

--- packages/web/src/pages/Home.tsx
+++ packages/web/src/pages/Home.tsx
@@ -65,7 +65,7 @@ export default function Home() {
           <VStack spacing={4} textAlign="center" color="white">
             <Heading size="2xl">Explore Your Perfect Vacation Escape</Heading>
             <Text fontSize="xl">
-              Find the best hiding spots for your next getaway
+              Escape the bounds of space and time with the perfect hiding spots for your next getaway.
             </Text>
 
           </VStack>
